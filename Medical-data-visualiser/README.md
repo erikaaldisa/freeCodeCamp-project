e72ab03538b82404ce8f1b81b9b1ba131af27395
Erika Aldisa <aldisaerika@gmail.com>
2023-03-10 11:39:59 +0800

    add medical data visualizer project

---
+++ Medical-data-visualiser/README.md
@@ -0,0 +1,13 @@
+<h1 class="text-center big-heading">xxx</h1>
+
+
+
+Unit tests are written for you under ```test_module.py```.
+
+<h3 class="text-center big-heading">Development</h3>
+
+For development, you can use ```main.py``` to test your calculate() function. Click the "run" button and ```main.py``` will run.
+
+<h3 class="text-center big-heading">Testing</h3>
+
+We imported the tests from ```test_module.py``` to ```main.py``` for your convenience. The tests will run automatically whenever you hit the "run" button.
---
+++ Medical-data-visualiser/medical_data_visualizer.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Import data
+df = pd.read_csv("medical_examination.csv")
+
+# Add 'overweight' column
+bmi = df["weight"]/np.square(df["height"]*0.01)
+df['overweight'] = np.where(bmi>25, 1, 0)
+
+# Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
+df['cholesterol'] = np.where(df['cholesterol']>1, 1, 0)
+df['gluc'] = np.where(df['gluc']>1, 1, 0)
+
+# Draw Categorical Plot
+def draw_cat_plot():
+    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+    col_names = ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=col_names)
+
+
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+    df_cat = df_cat.value_counts().reset_index(name="total") 
+
+    # Draw the catplot with 'sns.catplot()'
+    sns.catplot(data=df_cat,
+            x="variable",
+            y="total",
+            col="cardio",
+            hue="value",
+            kind="bar"
+            ).fig
+
+    # Get the figure for the output
+    fig = sns.catplot(data=df_cat,
+            x="variable",
+            y="total",
+            col="cardio",
+            hue="value",
+            kind="bar",
+            order=sorted(col_names)
+            ).fig
+
+    # Do not modify the next two lines
+    fig.savefig('catplot.png')
+    return fig
+
+
+# Draw Heat Map
+def draw_heat_map():
+    # Clean the data
+    df_heat = df[
+    (df['ap_lo'] <= df['ap_hi']) &
+    (df['height']>=df['height'].quantile(0.025)) &
+    (df['height']<=df['height'].quantile(0.975)) &
+    (df['weight']>=df['weight'].quantile(0.025)) &
+    (df['weight']<=df['weight'].quantile(0.975))]
+
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
+
+
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12, 9))
+
+    # Draw the heatmap with 'sns.heatmap()'
+    ax = sns.heatmap(corr,
+                    mask=mask,
+                    annot=True, 
+                    fmt='.1f',
+                    center=0,
+                    vmin=-0.5, 
+                    vmax=0.5)
+
+    # Do not modify the next two lines
+    fig.savefig('heatmap.png')
+    return fig
